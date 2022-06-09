6cae6aef4b1db468a264cd2115aba2aeb16b8b0c
Hugo Gonzalez <pinelo93@gmail.com>
2022-06-09 00:38:09 -0500

    Updates config for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Modulo de Laboratorio',
   tagline: 'Documentacion',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://hugogzz93.github.io/',
+  baseUrl: '/frisa-lab-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'hugogzz93', // Usually your GitHub org/user name.
+  projectName: 'frisa-lab-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
