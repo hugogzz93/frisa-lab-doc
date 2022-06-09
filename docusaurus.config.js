b8f1b4cb128e7c12e70bf5d69e22f651e69ff056
Hugo Gonzalez <pinelo93@gmail.com>
2022-06-09 17:25:11 -0500

    Changes landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -139,7 +139,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: '',
       },
       prism: {
         theme: lightCodeTheme,
